01b4803bff937cca91ec01a67fa163853d3ff261
Wesley <wesleyhokage123@outlook.com>
2021-02-06 16:04:18 -0300

    Basic System
    The basic system of Level Selector

---
+++ LevelSelector/Assets/LevelSelector/Scripts/LevelController/LevelController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    public void EnterInLevel(int valueLevel) => SceneManager.LoadScene(valueLevel);
+}
